b0705dd32afb2b6ddb2d6efb650f30703f6231b8
facudemarco <fac.demarco37@gmail.com>
2025-04-12 11:35:36 -0300

    testing

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ app.add_middleware(
 
 @app.get('/')
 def read_root():
-    return {"message": "Hello World"}
+    return {"message": "Serena Viajes API by iWeb Technology. All Rights reserved"}
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
